26edf6616a8c4595f479fcebbd3f5350dda5e75c
mtusidon <richtersscales@gmail.com>
2018-08-03 16:14:36 -0400

    Cleaned up, placed redundant code in single function, maybe only render to cite.iUh30 to avoid if/else block

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import TabChild from './TabChild.js'
 
-// Get the element to prepend our app to from https://www.google.com.
-// This could be a specific element on a website or something more general like `document.body`.
+// Parse for URL from single search result
 function scrapeURL(node){
   let iSub = '<a href="';
   let eSub = " ping";
@@ -13,54 +12,34 @@ function scrapeURL(node){
   let end = node.innerHTML.search(eSub) - 1;
   return node.innerHTML.substring(start, end);
 }
+// Render single action arrow 
+function createTabDiv (div, node){
+  div.id = 'injLink' + count;
+  let divContainer = document.createElement('div');
+  divContainer.id = 'injLinkDiv' + count;
+  if (div) div.appendChild(divContainer);
+  ReactDOM.render(<TabChild link={scrapeURL(node)}/>, document.getElementById('injLinkDiv'+count))
+}
 
+// Render Arrows for each displayed Link
+const divrCollections = document.querySelectorAll('div.r');
+let count = 0;
+for (let item of divrCollections) {
+    // If link does not have cache/similar action arrow
+    if (item.querySelector('span') == null) {
+      let buttonDiv = item.querySelector('cite.iUh30')
+      createTabDiv(buttonDiv, item);
+      count +=1;
+    }
+    else {
+      let buttonDiv = item.querySelector('span');
+      createTabDiv(buttonDiv, item);
+      count +=1;
+    }
+}
+// Render Comments Div
 const commentsDiv = document.getElementById('rcnt');
 const app = document.createElement('div');
 app.id = 'root';
 if (commentsDiv) commentsDiv.prepend(app);
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const divrCollections = document.querySelectorAll('div.r');
-const divrURLs = []
-let count = 0;
-for (let item of divrCollections) {
-    divrURLs.push(scrapeURL(item));
-    if (item.querySelector('span') == null) {
-      let buttonDiv = item.querySelector('cite.iUh30')
-      buttonDiv.id = 'injLink' + count;
-      let buttonDivContainer = document.createElement('div');
-      buttonDivContainer.id = 'injLinkDiv' + count;
-      if (buttonDiv) buttonDiv.appendChild(buttonDivContainer);
-      ReactDOM.render(<TabChild link={scrapeURL(item)}/>, document.getElementById('injLinkDiv'+count))
-      count +=1;
-    }
-    else {
-      let buttonDiv = item.querySelector('span');
-      buttonDiv.id = 'injLink' + count;
-      let buttonDivContainer = document.createElement('div');
-      buttonDivContainer.id = 'injLinkDiv' + count;
-      if (buttonDiv) buttonDiv.appendChild(buttonDivContainer);
-      ReactDOM.render(<TabChild link={scrapeURL(item)}/>, document.getElementById('injLinkDiv'+count))
-      count +=1;}
-
-
-}
-
-console.log("=== raw =======================");
-console.log(divrCollections);
-console.log("=== refined ===================");
-console.log(divrURLs);
-
-/* Create a div to render the App component to.
- Set the app element's id to `root`.
- This name is the same as the element that create-react-app renders to by default
- so it will work on the development server too.
- Prepend the App to the viewport element in production if it exists on the page.
- You could also use `appendChild` depending on your needs.
- Render the App.  */
-
-//    const commentsDiv = document.getElementById('rcnt');
-//    const app = document.createElement('div');
-//    app.id = 'root';
-//    if (commentsDiv) commentsDiv.prepend(app);
-//    ReactDOM.render(<App />, document.getElementById('root'));
